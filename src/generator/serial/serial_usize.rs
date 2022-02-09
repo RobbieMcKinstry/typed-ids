a8357adb3bd2d9ee321aa9ad7024fa0926f05132
Robbie McKinstry <thesnowmancometh@gmail.com>
2022-02-09 17:42:22 -0500

    Add generator type for serials (#4)
    * Add generator type for serials
    
    * Bump crate version

---
+++ src/generator/serial/serial_usize.rs
@@ -0,0 +1,57 @@
+use crate::generator::Generator;
+use crate::SerialUsize;
+
+pub struct SerialUsizeGenerator<T: 'static> {
+    gen: Generator<SerialUsize<T>>,
+}
+
+impl<T: 'static> SerialUsizeGenerator<T> {
+    pub fn new() -> Self {
+        // Create a closure which captures an integer counter.
+        // This closure provides the generation logic used
+        // by the underlying Generator type.
+        let mut counter = 0usize;
+        let closure = move || {
+            counter += 1;
+            SerialUsize::try_from(counter).unwrap()
+        };
+        // Use this closure for generation logic.
+        let gen = Generator::new(closure);
+        Self { gen }
+    }
+
+    pub async fn next(&self) -> SerialUsize<T> {
+        self.gen.next().await
+    }
+
+    pub async fn next_sync(&self) -> SerialUsize<T> {
+        self.gen.next_sync()
+    }
+}
+
+impl<T: 'static> Default for SerialUsizeGenerator<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::SerialUsizeGenerator;
+    use crate::SerialUsize;
+    use pretty_assertions::assert_eq;
+    use tokio::runtime::Runtime;
+
+    #[test]
+    fn generate_id() {
+        let rt = Runtime::new().unwrap();
+        rt.block_on(async {
+            let generator: SerialUsizeGenerator<()> = Default::default();
+            for i in 1..11 {
+                let expected: SerialUsize<()> = SerialUsize::try_from(i).unwrap();
+                let observed = generator.next().await;
+                assert_eq!(expected, observed);
+            }
+        })
+    }
+}
