30075d281a1ba5cd4a2c2c39d833aa68d0784bd5
Robbie McKinstry <thesnowmancometh@gmail.com>
2022-02-09 17:31:53 -0500

    Add generator type for serials

---
+++ src/generator/serial/mod.rs
@@ -0,0 +1,7 @@
+pub use serial_u32::SerialU32Generator;
+pub use serial_u64::SerialU64Generator;
+pub use serial_usize::SerialUsizeGenerator;
+
+mod serial_u32;
+mod serial_u64;
+mod serial_usize;
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
+pub use generator::{SerialU32Generator, SerialU64Generator, SerialUsizeGenerator};
 pub use id::Id;
-pub use serial::SerialU64;
+pub use serial::{SerialU32, SerialU64, SerialUsize};
 
+mod generator;
 mod id;
 mod serial;
